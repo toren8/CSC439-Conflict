bc7b03087dff532de97e14f239403e8337305d5d
austinh1 <codexscript1@gmail.com>
2015-01-27 15:38:16 -0500

    Just kidding, this is the actual commit

--- src/com/merge/conflict/Hello.java
+++ src/com/merge/conflict/Hello.java
@@ -3,11 +3,12 @@ package com.merge.conflict;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Team member 1, I changed it! I'm a code developer now.");
+		System.out.println("Team member 1 is a jerk!");
 		System.out.println("Team member 1: b");
-		System.out.println("Team member 2 ist der Hammer. It's a complement, I promise");
+		System.out.println("Team member 2 is the best!");
 		System.out.println("Team member 2: 2");
-		System.out.println("asdfklajsdlfjalksjdflkajsd;lkjfa;klsjfdklsjdflkja");
+		System.out.println("I am serious, they are terrible!");
+
 	}
 	
 }
