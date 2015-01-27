960961208fe0428067df7d239dc877b9c4313af8
Nick Meyer <nmeyer@intelemage.com>
2015-01-26 20:33:55 -0500

    Updating the file as member one.
    I am updating as team member one because I am awesome.

--- src/com/merge/conflict/Hello.java
+++ src/com/merge/conflict/Hello.java
@@ -3,11 +3,11 @@ package com.merge.conflict;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Team member 1: a");
+		System.out.println("Team member 1 is awesome!!!!");
 		System.out.println("Team member 1: b");
-		System.out.println("Team member 2: 1");
+		System.out.println("Team member 2 is the worst developer ever!");
 		System.out.println("Team member 2: 2");
-		System.out.println("Team member changes this line entirely");
+		System.out.println("Just kidding, we both are great!");
 	}
 	
 }
