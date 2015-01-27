90c540c907198effa096d2ce4efcdf2bd112112b
austinh1 <codexscript1@gmail.com>
2015-01-27 15:27:50 -0500

    Second members commit. Hi nick!

--- src/com/merge/conflict/Hello.java
+++ src/com/merge/conflict/Hello.java
@@ -3,11 +3,11 @@ package com.merge.conflict;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Team member 1: a");
+		System.out.println("Team member 1, I changed it! I'm a code developer now.");
 		System.out.println("Team member 1: b");
-		System.out.println("Team member 2: 1");
+		System.out.println("Team member 2 ist der Hammer. It's a complement, I promise");
 		System.out.println("Team member 2: 2");
-		System.out.println("Team member changes this line entirely");
+		System.out.println("asdfklajsdlfjalksjdflkajsd;lkjfa;klsjfdklsjdflkja");
 	}
 	
 }
